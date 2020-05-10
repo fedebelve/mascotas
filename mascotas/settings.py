ac30a5db433d57c28fdce1e5eb10367b02ba25ba
Federico Belvedere <federico.belvedere@gmail.com>
2020-05-10 19:36:25 -0400

    django-heroku

--- mascotas/settings.py
+++ mascotas/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
 from django.urls import reverse_lazy
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('adopcion:solicitud_listar')
+
+django_heroku.settings(locals())
